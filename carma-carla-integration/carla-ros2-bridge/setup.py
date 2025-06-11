e4e0b386fdbae225c1f032c8fbbca500d3a4d555
Will Varner <willv678@gmail.com>
2025-06-11 17:18:45 -0400

    setup.py update

--- carma-carla-integration/carla-ros2-bridge/setup.py
+++ carma-carla-integration/carla-ros2-bridge/setup.py
@@ -1,43 +1,35 @@
-### TO BE UPDATED LATER ###
-
-"""
-Setup for carla_ros_bridge
-"""
+from setuptools import find_packages, setup
 import os
 from glob import glob
-ROS_VERSION = int(os.environ['ROS_VERSION'])
 
-if ROS_VERSION == 1:
-    from distutils.core import setup
-    from catkin_pkg.python_setup import generate_distutils_setup
+package_name = 'carla_ros2_bridge' 
 
-    d = generate_distutils_setup(packages=['carla_ros_bridge'], package_dir={'': 'src'})
-
-    setup(**d)
-
-elif ROS_VERSION == 2:
-    from setuptools import setup
-
-    package_name = 'carla_ros_bridge'
-    setup(
-        name=package_name,
-        version='0.0.0',
-        packages=[package_name],
-        data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-                    ('share/' + package_name, ['package.xml']),
-                    (os.path.join('share', package_name), glob('launch/*.launch.py')),
-                    (os.path.join('share', package_name + '/test'), glob('test/test_objects.json'))],
-        install_requires=['setuptools'],
-        zip_safe=True,
-        maintainer='CARLA Simulator Team',
-        maintainer_email='carla.simulator@gmail.com',
-        description='CARLA ROS2 bridge',
-        license='MIT',
-        tests_require=['pytest'],
-        entry_points={
-            'console_scripts': ['bridge = carla_ros_bridge.bridge:main'],
-        },
-        package_dir={'': 'src'},
-        package_data={'': ['CARLA_VERSION']},
-        include_package_data=True
-    )
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(where='src'), 
+    package_dir={'': 'src'},
+    
+    data_files=[
+        # Install marker file for ament resource index
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]), 
+        # Install package.xml
+        ('share/' + package_name, ['package.xml']),
+        # Install all .launch.py files from the 'launch' directory
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Will Varner',
+    maintainer_email='will.varner@uga.edu',
+    description='Ported CARLA ROS 2 bridge for CDASim.',
+    license='MIT',
+    tests_require=['pytest'],
+    
+    entry_points={
+        'console_scripts': [
+            'bridge = carla_ros_bridge.carla_bridge_node:main',
+        ],
+    },
+)
